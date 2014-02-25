0788de5cc6aba6d67f0db07eaeadae80fd02a094
Favio Manriquez Leon <favio@favrik.com>
2014-02-24 23:58:31 -0800

    Add coffee script task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
 var qunit = require('gulp-qunit');
 
@@ -6,5 +7,14 @@ gulp.task('test', function() {
   return gulp.src('./test/runner.html').pipe(qunit());
 });
 
+gulp.task('build', function () {
+  try {
+    gulp.src('./src/*.coffee')
+        .pipe(coffee({bare: true}).on('error', gutil.log))
+        .pipe(gulp.dest('./build/'));
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 gulp.task('default', ['test']);
